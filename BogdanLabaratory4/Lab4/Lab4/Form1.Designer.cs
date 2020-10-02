7a60200809412f79ee334ff116e448e3157b3706
Miger4ik <46415946+Miger4ik@users.noreply.github.com>
2020-10-02 19:00:24 +0300

    add lab4

---
+++ BogdanLabaratory4/Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonIntegrate = new System.Windows.Forms.Button();
+            this.numericA = new System.Windows.Forms.NumericUpDown();
+            this.numericB = new System.Windows.Forms.NumericUpDown();
+            this.numericH = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(856, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(856, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(856, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "H";
+            // 
+            // buttonIntegrate
+            // 
+            this.buttonIntegrate.Location = new System.Drawing.Point(645, 552);
+            this.buttonIntegrate.Name = "buttonIntegrate";
+            this.buttonIntegrate.Size = new System.Drawing.Size(237, 54);
+            this.buttonIntegrate.TabIndex = 6;
+            this.buttonIntegrate.Text = "Integrate";
+            this.buttonIntegrate.UseVisualStyleBackColor = true;
+            this.buttonIntegrate.Click += new System.EventHandler(this.buttonIntegrate_Click);
+            // 
+            // numericA
+            // 
+            this.numericA.Location = new System.Drawing.Point(645, 75);
+            this.numericA.Name = "numericA";
+            this.numericA.Size = new System.Drawing.Size(120, 31);
+            this.numericA.TabIndex = 7;
+            // 
+            // numericB
+            // 
+            this.numericB.Location = new System.Drawing.Point(645, 143);
+            this.numericB.Name = "numericB";
+            this.numericB.Size = new System.Drawing.Size(120, 31);
+            this.numericB.TabIndex = 8;
+            // 
+            // numericH
+            // 
+            this.numericH.Location = new System.Drawing.Point(645, 214);
+            this.numericH.Name = "numericH";
+            this.numericH.Size = new System.Drawing.Size(120, 31);
+            this.numericH.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 683);
+            this.Controls.Add(this.numericH);
+            this.Controls.Add(this.numericB);
+            this.Controls.Add(this.numericA);
+            this.Controls.Add(this.buttonIntegrate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonIntegrate;
+        private System.Windows.Forms.NumericUpDown numericA;
+        private System.Windows.Forms.NumericUpDown numericB;
+        private System.Windows.Forms.NumericUpDown numericH;
+    }
+}
+
---
+++ BogdanLabaratory4/Lab4/Lab4/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        private double a = 0;
+        private double b = 0;
+        private double h = 0;
+
+        private IIntegral integralRectangles = new IntegralRectangles();
+        private IIntegral integralTrapezoid = new IntegralTrapezoid();
+        private IIntegral integralSimpson = new IntegralSimpson();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonIntegrate_Click(object sender, EventArgs e)
+        {
+            a = (double)numericA.Value;
+            b = (double)numericB.Value;
+            h = (double)numericH.Value;
+
+            integralRectangles.Integrate(a, b, h);
+            integralTrapezoid.Integrate(a, b, h);
+            integralSimpson.Integrate(a, b, h);
+        }
+    }
+
+    public interface IIntegral
+    {
+        void Integrate(double a, double b, double h);
+    }
+
+    public class IntegralRectangles : IIntegral
+    {
+        public void Integrate(double a, double b, double h)
+        {
+            
+        }
+    }
+
+    public class IntegralTrapezoid : IIntegral
+    {
+        public void Integrate(double a, double b, double h)
+        {
+
+        }
+    }
+
+    public class IntegralSimpson : IIntegral
+    {
+        public void Integrate(double a, double b, double h)
+        {
+
+        }
+    }
+}
